259cbaac85b157ddef521f3a8501d9ae210951f7
D-Jester <pingking29705@gmail.com>
2025-03-25 12:08:29 +0530

    modified the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,74 @@
 "use client";
 
-import { AppBar, Toolbar, Typography, Box, Button, Avatar } from "@mui/material";
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, Box, Button, Avatar, Menu, MenuItem } from "@mui/material";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-export default function Navbar({  }) {
-  const user=useSelector((state)=>state.user);
-  console.log(user);
+export default function Navbar() {
+  const user = useSelector((state) => state.user);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="fixed" sx={{ bgcolor: "#b70924" }}>
       <Toolbar sx={{ minHeight: "50px", display: "flex", justifyContent: "space-between" }}>
-        {/* Left: User Full Name */}
+        {/* Left: Logo or Title */}
         <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-         JUMERP
+          JUMERP
         </Typography>
 
-        {/* Right: Sign In, Sign Up, and User Avatar */}
+        {/* Center: Navigation Links */}
         <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-          {/* <Button color="inherit">Sign In</Button>
-          <Button color="inherit">Sign Up</Button> */}
-             <Typography  sx={{ fontWeight: "bold", fontSize:"15px" }}>
-         {user.fullName}
-        </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/about">
+            About
+          </Button>
+          <Button color="inherit" component={Link} to="/facilities">
+            Facilities
+          </Button>
+          <Button color="inherit" component={Link} to="/faculty">
+            Faculty
+          </Button>
+          <Button color="inherit" component={Link} to="/contact">
+            Contact
+          </Button>
+        </Box>
+
+        {/* Right: User Full Name and Avatar */}
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Typography sx={{ fontWeight: "bold", fontSize: "15px" }}>
+            {user.fullName}
+          </Typography>
           <Avatar 
             src={user?.avatar || "/default-avatar.png"} 
             alt={user?.fullName} 
-            sx={{ width: 40, height: 40 }}
+            sx={{ width: 40, height: 40, cursor: "pointer" }}
+            onClick={handleMenuOpen}
           />
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem component={Link} to="/auth" onClick={handleMenuClose}>
+              Login
+            </MenuItem>
+            <MenuItem component={Link} to="/admin-login" onClick={handleMenuClose}>
+              Administrative Login
+            </MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
-    
   );
 }
